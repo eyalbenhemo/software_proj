66e0a169419d18f56e680da120132c523ce327d3
eyal ben hemo <eyalbenhemo@gmail.com>
2020-12-20 00:05:30 +0200

    change get_probs to be much faster

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -24,20 +24,21 @@ assert (K is not None and N is not None and d is not None and MAX_ITER is not No
 assert K > 0 and N > 0 and d > 0 and MAX_ITER > 0, "K, N, d, MAX_ITER Have to be positive"
 assert K < N, "K must be less than N"
 
-#Init data frame with the input file
+# Init data frame with the input file
 df = pd.read_csv(filename, header=None)
 df = pd.DataFrame.to_numpy(df, dtype=np.float64)
 
-#Init the centroids randomly by the Algorithem given
-#Call using C-API to Kmeans Algorithem from HW1
-#Print the initials centroids' indexes and the result of Kmeans
+
+# Init the centroids randomly by the Algorithem given
+# Call using C-API to Kmeans Algorithem from HW1
+# Print the initials centroids' indexes and the result of Kmeans
 def k_means_pp():
     np.random.seed(0)
     centroids = np.zeros(K, dtype=int)
     centroids[0] = np.random.choice(N, 1)
+    distances = np.zeros(N)
     for j in range(1, K):
-        probs = get_probs(centroids, j)
-        centroids[j] = np.random.choice(N, 1, p=probs)
+        centroids[j] = np.random.choice(N, 1, p=get_probs(centroids, j, distances))
     centroids = np.ndarray.tolist(centroids)
     print(",".join([str(centroid) for centroid in centroids]))
     res = msp.calc_centroids(K, N, d, MAX_ITER, np.ndarray.tolist(df), centroids)
@@ -45,20 +46,21 @@ def k_means_pp():
         temp = ["{:.16f}".format(np.float64(c)) for c in res[i]]
         print(*temp, sep=",")
 
-#Calc the prob for every observition by it's distance from the closest centroid
-def get_probs(centroids, j):
-    probs = np.zeros(N)
-    index = 0
-    for row in df:
-        mindist = np.linalg.norm(row - df[centroids[0]]) ** 2
-        for i in range(1, j):
-            candidate = np.linalg.norm(row - df[centroids[i]]) ** 2
-            if candidate < mindist:
-                mindist = candidate
-        probs[index] = mindist
-        index += 1
-    probs = probs / np.sum(probs)
-    return probs
 
-#The Execution of the Algorithem
+# Calc the prob for every observition by it's distance from the closest centroid
+def get_probs(centroids, j, distances):
+    index = 0
+    if j == 1:
+        for row in df:
+            distances[index] = np.linalg.norm(row - centroids[j]) ** 2
+            index += 1
+    else:
+        for row in df:
+            candidate = np.linalg.norm(row - centroids[j]) ** 2
+            if candidate < distances[index]:
+                distances[index] = candidate
+            index += 1
+    return distances / np.sum(distances)
+
+# The Execution of the Algorithem
 k_means_pp()
