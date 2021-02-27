338d14db03a571fad1ffc5e79c05be39989d3dfa
Eyal Ben-Hemo <eyalb@global.cadence.com>
2021-02-27 21:13:49 +0200

    done small change in step 1

--- normalized_specrtal.py
+++ normalized_specrtal.py
@@ -8,27 +8,29 @@ def norm_spect_clustering():
     df = data.df
     N = data.N
     W = weighted_adjacency(N, df)
-    np.set_printoptions(precision=1)
-    # print(W)
+    # np.set_printoptions(precision=1)
+    print(1)
     D = diagonal_mat_minus_sqrt(N, W)
-    # print(D)
+    print(2)
     L = norm_laplacian(N, W, D)
-    print(L)
+    print(3)
 
 
 # step 1
 def weighted_adjacency(N, df):
-    mat = np.array([weight_foo(df[i], df[j], i, j) for i in range(N) for j in range(N)], dtype=np.float64)
+    mat = np.array([weight_foo(x_i, x_j) for x_i in df for x_j in df], dtype=np.float64)
     mat.shape = (N, N)
-    return mat
+    return mat - np.identity(N)
 
 
-def weight_foo(x_i, x_j, i, j):
-    return 0 if i == j else math.exp(-0.5 * (np.linalg.norm(x_i - x_j) ** 2))
+def weight_foo(x_i, x_j):
+    return math.exp(-0.5 * (np.linalg.norm(x_i - x_j) ** 2))
+
 
 # step 2
 def diagonal_mat_minus_sqrt(N, W):
     return np.diag(np.array([math.pow(np.sum(W[i]), -0.5) for i in range(N)]))
+
 
 def norm_laplacian(N, W, D):
     return np.identity(N) - np.dot(np.dot(D, W), D)
